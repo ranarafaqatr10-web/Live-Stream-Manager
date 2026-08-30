759cad2468b313c181ec560725eecb0d56f0dd84
ranarafaqatr10-web <ranarafaqatr10@gmail.com>
2026-08-31 01:02:52 +0500

    Remove duplicate service worker event listeners
    Removed duplicate event listeners for 'install' and 'fetch'.

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', (e) => {
+  console.log('Service Worker Installed');
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request));
+});
+self.addEventListener('install', (e) => {
+  console.log('Service Worker Installed');
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request));
+});
+
